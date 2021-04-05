6a0f6401b1c78f89e40f71693ccaf3765edd9c8e
mmenault <maxime.menault@orange.fr>
2021-04-05 19:06:53 +0200

    .....

--- PMA_BackEnd/src/PMA_Core/FightController.py
+++ PMA_BackEnd/src/PMA_Core/FightController.py
@@ -1,21 +1,55 @@
+import random
+import time
+
+# Import bugué
+
+from ..PMA_Character.Character import *
+from ..PMA_Item.Weapon import *
+
 def fight(allie,enemy):
     while allie.isAlive() and enemy.isAlive():
-        weapon_damage = allie.equippedWeapon.attack()
+        print(allie.name+" : "+str(allie.HP)+" pv | "+enemy.name+" : "+str(enemy.HP)+ " pv")
+        weapon_damage = allie.equippedWeapon.getDamage()
         dice_value = fight_dices(weapon_damage)
-        enemy.hp = enemy.hp - dice_value
+        enemy.HP = enemy.HP - dice_value
+        print(allie.name+" a inflige "+str(dice_value)+" degats a "+enemy.name)
         if enemy.isAlive():
-            weapon_damage = enemy.equippedWeapon.attack()
+            weapon_damage = enemy.equippedWeapon.getDamage()
             dice_value = fight_dices(weapon_damage)
-            allie.hp = allie.hp - dice_value
+            allie.HP = allie.HP - dice_value
+            print(enemy.name+" a inflige "+str(dice_value)+" degats a "+allie.name)
     if allie.isAlive():
         print("C'est les gentils qui ont gagnés !")
     else:
         print("Ouch ! coup dur, t'as perdu")
 
-def fight_dices(dices):
+def throw_dice(dice):
+    return random.randint(1,dice)
 
+def fight_dices(dices):
+    damages = 0
     damage_type = dices[0]
     dices = dices[1:]
     Tab = dices.split('+')
     Tab = [i.split('d') for i in Tab]
-    print(Tab)
+    for i in Tab:
+        if len(i) == 2:
+            for j in range(int(i[0])):
+                damages += throw_dice(int(i[1]))
+        else:
+            damages += int(i[0])
+    return damages
+
+random.seed(time.time())
+jean = PlayableCharacter("Jean","Dwarf","Warrior")
+jean.setStats(16,12,16,6,6,8)
+jean.setMaxHP(20)
+epee = Weapon("Sword",15,"P1d8+3",1,1,"","")
+jean.equipWeapon(epee)
+
+jack = NonPlayableCharacter("Jack","Goblin", 0,"Spear")
+jack.setStats(8,8,10,4,4,6)
+jack.setMaxHP(20)
+jack.equipWeapon(epee)
+
+fight(jean,jack)
