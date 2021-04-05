34e53672ad7ed3d1120522b30fb0dcc78cc5823f
clement <clement.girard@etu.utc.fr>
2021-04-05 21:29:45 +0200

    Added multiplayer mode, added initiative draws and added target selection

--- PMA_BackEnd/src/PMA_Core/FightController.py
+++ PMA_BackEnd/src/PMA_Core/FightController.py
@@ -5,24 +5,25 @@ from PMA_BackEnd.src.PMA_Character.Character import *
 from PMA_BackEnd.src.PMA_Item.Weapon import *
 
 
-# Import bugué
+def fight(allies, enemies):
+    order = sort_initiatives(allies + enemies)
+    a, e = True, True
+    while a and e:
+        for i in order:
+            if i[1].isAlive():
+                target = select_target(enemies) if i[1] in allies else select_target(allies)
+                target.HP = target.HP - fight_dices(i[1].equippedWeapon.getDamage())
+        a = sum([1 if i.isAlive() else 0 for i in allies]) != 0
+        e = sum([1 if i.isAlive() else 0 for i in enemies]) != 0
+    if a: print("C'est les gentils qui ont gagnés !")
+    if e: print("Ouch ! coup dur, t'as perdu")
 
-def fight(allie, enemy):
-    while allie.isAlive() and enemy.isAlive():
-        print(allie.name + " : " + str(allie.HP) + " pv | " + enemy.name + " : " + str(enemy.HP) + " pv")
-        weapon_damage = allie.equippedWeapon.getDamage()
-        dice_value = fight_dices(weapon_damage)
-        enemy.HP = enemy.HP - dice_value
-        print(allie.name + " a inflige " + str(dice_value) + " degats a " + enemy.name)
-        if enemy.isAlive():
-            weapon_damage = enemy.equippedWeapon.getDamage()
-            dice_value = fight_dices(weapon_damage)
-            allie.HP = allie.HP - dice_value
-            print(enemy.name + " a inflige " + str(dice_value) + " degats a " + allie.name)
-    if allie.isAlive():
-        print("C'est les gentils qui ont gagnés !")
-    else:
-        print("Ouch ! coup dur, t'as perdu")
+
+def select_target(others):
+    others = others.copy()
+    for i in others:
+        if not i.isAlive(): others.remove(i)
+    return others[random.randint(0, len(others)-1)]
 
 
 def throw_dice(dice):
@@ -32,16 +33,16 @@ def throw_dice(dice):
 def fight_dices(dices):
     damages = 0
     damage_type = dices[0]
-    dices = dices[1:]
-    Tab = dices.split('+')
-    Tab = [i.split('d') for i in Tab]
-    for i in Tab:
-        if len(i) == 2:
-            for j in range(int(i[0])):
-                damages += throw_dice(int(i[1]))
-        else:
-            damages += int(i[0])
+    tab = [i.split('d') for i in dices[1:].split('+')]
+    for i in tab:
+        damages += sum([throw_dice(int(i[1]))] * int(i[0])) if len(i) == 2 else int(i[0])
     return damages
+
+
+def sort_initiatives(characters):
+    initiatives = [(throw_dice(20) + i.dexterity, i) for i in characters]
+    initiatives.sort()
+    return initiatives
 
 
 random.seed(time.time())
@@ -50,10 +51,12 @@ jean.setStats(16, 12, 16, 6, 6, 8)
 jean.setMaxHP(20)
 epee = Weapon("Sword", 15, "P1d8+3", 1, 1, "", "")
 jean.equipWeapon(epee)
+players = [jean]
 
 jack = NonPlayableCharacter("Jack", "Goblin", 0, "Spear")
 jack.setStats(8, 8, 10, 4, 4, 6)
 jack.setMaxHP(20)
 jack.equipWeapon(epee)
+monsters = [jack]
 
-fight(jean, jack)
+fight(players, monsters)
