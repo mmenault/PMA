176abba0e7740e53a152202bfd59d9dac2b7fc40
clement <clement.girard@etu.utc.fr>
2021-04-06 21:01:53 +0200

    Coding of the A* and usual class function

--- PMA_BackEnd/src/PMA_Core/Map.py
+++ PMA_BackEnd/src/PMA_Core/Map.py
@@ -1,32 +1,60 @@
-from math import pow, sqrt
+def getDistBetweenTwoCases(string, string2):
+    return abs(ord(string[:1]) - ord(string2[:1])) + abs(ord(string[1:]) - ord(string2[1:]))
+
+
+def getSons(mapE, string):
+    l1 = []
+    for i in ([-1, 1]):
+        w = chr(ord(string[:1]) + i) + string[1:]
+        b = string[:1] + chr(ord(string[1:]) + i)
+        l1.append(w) if w in mapE else None
+        l1.append(b) if b in mapE else None
+    return l1
 
 
 class Map:
-    def __init__(self, walls, grounds, characters, objets, size_x, size_y):
-        self.map = [[{'wall_size': walls[i][j], 'ground_coefficient': grounds[i][j]} for j in size_y] for i in size_x]
-        self.characters = characters
-        self.objets = objets
 
-    def getCharacter_XY(self, character):
-        for i in self.characters:
-            if i['Object'] == character:
-                return i['X'], i['Y']
+    def __init__(self, walls, entities, size_x, size_y):
+        self.mapOfEntities = {}
+        self.map = {}
+        for x in size_x:
+            for y in size_y:
+                case = chr(ord('A') + x) + chr(ord('1') + y)
+                if not (case in walls):
+                    if case in entities:
+                        self.mapOfEntities[case] = entities[case]
+                    else:
+                        self.mapOfEntities[case] = None
+                    self.map[case] = 0
+
+    def getMinWalkDistBetweenEntityAndCase(self, EntityOne, Case):
+        mapE = self.map.copy()
+        debut = self.getEntityCase(EntityOne)
+        fin = Case
+        li = {debut: getDistBetweenTwoCases(debut, fin)}
+        if debut in mapE: del mapE[debut]
+
+        while not (fin in li):
+            first_key = list(li.keys())[0]
+            li[first_key] = li[first_key] + 1
+
+            for a in getSons(mapE, first_key):
+                if not (a in li):
+                    li[a] = getDistBetweenTwoCases(a, fin) + (li[first_key] - getDistBetweenTwoCases(first_key, fin))
+                del mapE[a]
+
+            del li[first_key]
+
+            li = dict(sorted(li.items(), key=lambda x: x[1]))
+        for i in li: return li[i]
+
+    def getFlyDistanceBetweenEntityAndCase(self, EntityOne, Case):
+        debut = self.getEntityCase(EntityOne)
+        fin = self.getEntityCase(Case)
+        return getDistBetweenTwoCases(debut, fin)
+
+    def getEntityCase(self, entity):
+        for i in self.mapOfEntities:
+            if self.mapOfEntities[i] == entity:
+                return i
         return None
-
-    def getObject_XY(self, objet):
-        for i in self.objets:
-            if i['Object'] == objet:
-                return i['X'], i['Y']
-        return None
-
-    def getDirectViewTargetDistance(self, character, target):
-        X_Y_1 = self.getCharacter_XY(character)
-        X_Y_2 = self.getCharacter_XY(target) if self.getCharacter_XY(target) is not None else self.getObject_XY(target)
-        # TODO if no direct line (A* =! unobstructed manhattan's distance) return an infinite distance
-        return sqrt(pow(X_Y_1[0] - X_Y_2[0], 2) + pow(X_Y_1[1] - X_Y_2[1], 2))
-
-    # TODO Recode a A* in getDistanceToGoToTarget
-
-    # TODO Recode a modified A* in getNumberOfActionsToGoToTarget
-
-    # TODO Create the function to move for N actions to its target
